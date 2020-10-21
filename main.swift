d6feaaf3d20dc8b48119f9ddad3b99f2da2d441c
Muhammad Luthfi Farizqi Luthfi <luthfifarizqi23@gmail.com>
2020-10-21 02:57:09 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,10 @@
+//The ternary operator
+let firstCard = 11
+let secondCard = 10
+print(firstCard == secondCard ? "Cards are the same" : "SMK IDN ")
+
+if firstCard == secondCard {
+    print("Cards are the same")
+} else {
+    print("Jonggol")
+}
